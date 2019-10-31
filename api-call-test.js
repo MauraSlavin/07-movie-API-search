cd9984a6abe83b1a4f748bdf32162d9989e20bcb
David Hamilton <dahamilton10@yahoo.com>
2019-10-31 13:43:58 -0400

    fixed that calls to misix and tastedive

--- api-call-test.js
+++ api-call-test.js
@@ -3,25 +3,49 @@
 
 // ajax to the musixmatch api
 
-// $.ajax({
-//     method: "GET",
-//     url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433",
-//     Header: {
-//         "Access-Control-Allow-Origin":"*",
-//     }
-//     }).then(function(responce){
-//         console.log(responce);
-//     });
+function getLyrics() {
+
+    var artistSearch = "Billy Joel";
+    $.ajax({
+        type: "GET",
+        data: {
+            apikey: "2e226514eac6539c3e3fde46c74f0129",
+            q_artist: artistSearch,
+            format: "jsonp",
+            callback: "jsonp_callback"
+        },
+        url: "https://api.musixmatch.com/ws/1.1/track.search",
+        dataType: "jsonp",
+        jsonpCallback: 'jsonp_callback',
+        contentType: 'application/json',
+        success: function (data) {
+            console.log(data);
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+};
+
+
+getLyrics();
+
+
+//The totality of the was taken from https://codepen.io/brian_jenney/pen/dGKmyX
+// I have no idea whats going on
 
 
 // ajax to the tastedive api
 
-// $.ajax({
-//     url: "https://tastedive.com/api/similar?q=Black+Sabbath&k=348815-07musicA-UK0GNRNO",
-//     method: "GET"
-// }).then(function(responce){
-//     console.log(responce)
-// });
+$.ajax({
+    url: "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=Black+Sabbath&k=348815-07musicA-UK0GNRNO",
+    method: "GET"
+}).then(function (responce) {
+    console.log(responce)
+});
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,18 +56,18 @@
 
 // This is for the nyt movie review api
 
-$.ajax({
-    url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=lebowski&api-key=zZrGvMTHO8rZYgmqMozo6nBXMVSdTemM",
-    method: "GET"
-}).then(function(responce){
-    console.log(responce);
-});
+// $.ajax({
+//     url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=lebowski&api-key=zZrGvMTHO8rZYgmqMozo6nBXMVSdTemM",
+//     method: "GET"
+// }).then(function(responce){
+//     console.log(responce);
+// });
 
-// This is for omdb
+// // This is for omdb
 
-$.ajax({
-    url: "https://www.omdbapi.com/?t=lebowski&apikey=bdc51342",
-    method: "GET"
-}).then(function(responce){
-    console.log(responce);
-});
+// $.ajax({
+//     url: "https://www.omdbapi.com/?t=lebowski&apikey=bdc51342",
+//     method: "GET"
+// }).then(function(responce){
+//     console.log(responce);
+// });
