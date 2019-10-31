8383eb39b40c0bb2ce2cf8efbede06c15122fff5
David Hamilton <dahamilton10@yahoo.com>
2019-10-30 20:39:29 -0400

    Couldnt get the music apis to respond because of CORS errors. So to show good faith that I'm trying I put in some working movie apis that I can get a responce from

--- api-call-test.js
+++ api-call-test.js
@@ -2,18 +2,48 @@
 // the tastedive api key is 348815-07musicA-UK0GNRNO
 
 // ajax to the musixmatch api
+
 // $.ajax({
 //     method: "GET",
-//     url: "http://tracking.musixmatch.com/t1.0/2e226514eac6539c3e3fde46c74f0129/?artist_name=katy%20perry%20&track_name=hot%20n%20cold",
+//     url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433",
+//     Header: {
+//         "Access-Control-Allow-Origin":"*",
+//     }
 //     }).then(function(responce){
 //         console.log(responce);
 //     });
 
 
 // ajax to the tastedive api
+
+// $.ajax({
+//     url: "https://tastedive.com/api/similar?q=Black+Sabbath&k=348815-07musicA-UK0GNRNO",
+//     method: "GET"
+// }).then(function(responce){
+//     console.log(responce)
+// });
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// movie memes
+
+
+
+// This is for the nyt movie review api
+
 $.ajax({
-    url: "https://tastedive.com/api/similar?q=Black+Sabbath&k=348815-07musicA-UK0GNRNO",
+    url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=lebowski&api-key=zZrGvMTHO8rZYgmqMozo6nBXMVSdTemM",
     method: "GET"
 }).then(function(responce){
-    console.log(responce)
+    console.log(responce);
+});
+
+// This is for omdb
+
+$.ajax({
+    url: "https://www.omdbapi.com/?t=lebowski&apikey=bdc51342",
+    method: "GET"
+}).then(function(responce){
+    console.log(responce);
 });
