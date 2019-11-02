2de8f043318d665eda3a1f2f4bb6ec0d7e26caac
David Hamilton <dahamilton10@yahoo.com>
2019-11-02 11:24:11 -0400

    finished the user input search query

--- javascript.js
+++ javascript.js
@@ -47,7 +47,7 @@ $.ajax({
 // This is for the nyt movie review api
 
 $.ajax({
-    url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=space+jam&api-key=zZrGvMTHO8rZYgmqMozo6nBXMVSdTemM",
+    url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=lebowski&api-key=zZrGvMTHO8rZYgmqMozo6nBXMVSdTemM",
     method: "GET"
 }).then(function(responce){
     console.log(responce);
@@ -83,7 +83,15 @@ $.ajax({
 
 $("#search-button").on("click", function(event){
     event.preventDefault();
-    alert("hello");
+    inputMovie = $(".movie-input").val();
+
+    $.ajax({
+        url: `https://www.omdbapi.com/?t=${inputMovie}&y=&plot=short&apikey=trilogy`,
+        method: "GET"
+    }).then(function(responce){
+        console.log(responce);
+    });
+    
 
     // Maura:  This adds the elements in the new ul, but doesn't display it on the page, yet
 
